f9a201f4aae83885bee691ea0821c03588aa21e2
Sabeel Khan <sabeelhps@gmail.com>
2020-11-28 10:41:50 +0530

    second

--- test.cpp
+++ test.cpp
@@ -1,61 +1,31 @@
 #include<iostream>
-#include<vector>
-#include<algorithm>
-#include<unordered_map>
-#include<map>
-#include<stack>
-#include<queue>
-#include<set>
-#include<cstring>
-#include<cmath>
-
 using namespace std;
 
-#define int long long int
+int main(){
+
+    char a[1000];
+    cin>>a;
+
+    if(a[0]!='9'){
+        int temp=(9-(a[0]-'0'));
+        if(a[0]-'0'>temp){
+            a[0]=temp+'0';
+        }
+    }
+
+    for(int i=1;a[i]!='\0';i++){
+        int temp=9-(a[i]-'0');
+
+        if(a[i]-'0'>temp){
+            a[i]=temp+'0';
+        }
+    }
+
+    for(int i=0;a[i]!='\0';i++){
+        cout<<a[i];
+    }
+    cout<<endl;
 
 
-int solve(vector<vector<int> >v){
-
-	vector<int>ans,a,b;
-
-	a=v[0];
-	b=v[1];
-
-	ans.resize(a.size()+b.size());
-	merge(a.begin(),a.end(),b.begin(),b.end(),ans.begin());
-
-	for(int i=2;i<v.size();i++){
-
-		b.clear();
-		b=v[i];
-		a.swap(ans);
-		ans.clear();
-		ans.resize(a.size()+b.size());
-		merge(a.begin(),a.end(),b.begin(),b.end(),ans.begin());
-
-	}
-
-	int median=ans[ans.size()/2];
-
-	return median;
-}
-
-int32_t main(){
-
-	int row,col;
-	cin>>row>>col;
-	vector<vector<int> >v(row,vector<int>(col,0));
-	
-	int d;
-	for(int i=0;i<row;i++){
-		for(int j=0;j<col;j++){
-			cin>>d;
-			v[i][j]=d;
-		}
-	}
-
-	cout<<solve(v)<<endl;
-
-
-	return 0;
+    return 0;
 }
