a5e90f76f638ee9aa7620ec82db96478265b03de
Sabeel Khan <sabeelhps@gmail.com>
2020-12-01 16:57:32 +0530

    enjoycoding

--- test.cpp
+++ test.cpp
@@ -2,30 +2,13 @@
 using namespace std;
 
 int main(){
+ 	
 
-    char a[1000];
-    cin>>a;
+ 	int n=5;
 
-    if(a[0]!='9'){
-        int temp=(9-(a[0]-'0'));
-        if(a[0]-'0'>temp){
-            a[0]=temp+'0';
-        }
-    }
+ 	char ch=n+'0';
 
-    for(int i=1;a[i]!='\0';i++){
-        int temp=9-(a[i]-'0');
-
-        if(a[i]-'0'>temp){
-            a[i]=temp+'0';
-        }
-    }
-
-    for(int i=0;a[i]!='\0';i++){
-        cout<<a[i];
-    }
-    cout<<endl;
-
-
-    return 0;
+ 	cout<<ch<<endl; 
+ 
+ return 0;
 }
